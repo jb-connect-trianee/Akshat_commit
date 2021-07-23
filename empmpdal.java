1789488002900847f631fe3b9c93093c0b4140ac
Akshat Goyal <54413209+akshatgoyal-cs@users.noreply.github.com>
2021-07-23 11:30:49 +0530

    Create empmpdal.java

---
+++ empmpdal.java
@@ -0,0 +1,25 @@
+package com.demo.json1;
+
+public class EmpModel {
+    private int empId;
+    private String name;
+    private String notes;
+    public int getEmpId() {
+        return empId;
+    }
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
